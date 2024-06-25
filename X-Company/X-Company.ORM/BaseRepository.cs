5bdd69df9692665e72269619dec150b899663cd3
pedro-ca <pedro.henric.rosario@gmail.com>
2024-06-25 19:09:35 -0300

    Implementing ORM layer

---
+++ X-Company/X-Company.ORM/BaseRepository.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using X_Company.Domain;
+
+namespace X_Company.ORM
+{
+    public class BaseRepository<T> where T : BaseEntity
+    {
+        protected readonly XCompanyDBContext db;
+        protected readonly DbSet<T> dbSet;
+
+        protected BaseRepository(XCompanyDBContext db)
+        {
+            this.db = db;
+            dbSet = db.Set<T>();
+        }
+
+        public bool Insert(T registro)
+        {
+            try
+            {
+                dbSet.Add(registro);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public virtual bool Update(int id, T registro)
+        {
+            try
+            {
+                var entity = dbSet.Find(id);
+                db.Entry(entity).CurrentValues.SetValues(registro);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                T registro = SelectById(id);
+                if (registro != null)
+                {
+                    dbSet.Remove(registro);
+                    db.SaveChanges();
+                }
+                else
+                    return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+        public virtual T SelectById(int id)
+        {
+            try
+            {
+                return dbSet.FirstOrDefault(x => x.Id.Equals(id));
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public virtual List<T> SelectAll()
+        {
+            try
+            {
+                return dbSet.ToList<T>();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public bool Exists(int id)
+        {
+            try
+            {
+                return dbSet.ToList().Exists(x => x.Id == id);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ X-Company/X-Company.ORM/XCompanyDBContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using X_Company.Domain.Features;
+
+namespace X_Company.ORM
+{
+    public class XCompanyDBContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(null)
+                .UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBLocadoraDeVeiculosORM;Integrated Security=True");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(XCompanyDBContext).Assembly);
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(decimal));
+
+                foreach (var property in properties)
+                    if (string.IsNullOrEmpty(property.GetColumnType()) && !property.GetMaxLength().HasValue)
+                        property.SetColumnType("decimal(18,2)");
+            }
+        }
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
+            return config;
+        }
+    }
+}
